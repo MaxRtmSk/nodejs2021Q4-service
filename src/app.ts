b56976bc66cb4ee19eb86ee31b24ec9f1c05a522
ratomsky <max.work.dev@gmail.com>
2021-12-19 21:32:17 +0300

    feat: change tsconfig and settings swagger

--- src/app.ts
+++ src/app.ts
@@ -1,22 +1,23 @@
 import { Server, IncomingMessage, ServerResponse } from 'http';
-import fastify, {FastifyInstance} from 'fastify';
-
+import fastify, { FastifyInstance, FastifyPluginOptions } from 'fastify';
+import fastifySwagger from 'fastify-swagger';
 
 import userRoutes from './resources/user/user.router';
-
-const boardRoute = require('./resources/board/board.router');
-const tasksRoute = require('./resources/tasks/tasks.router');
+import boardRoute from './resources/board/board.router';
+import tasksRoute from './resources/tasks/tasks.router';
 
 
 const server: FastifyInstance<Server, IncomingMessage, ServerResponse> = fastify({ logger: true });
 
-server.register(require('fastify-swagger'), {
+const swaggerOptions = {
   exposeRoute: true,
   routePrefix: '/api-docs',
   swagger: {
     info: {title: 'REST API'}
   }
-});
+} as FastifyPluginOptions
+
+server.register(fastifySwagger, swaggerOptions);
 
 server.register(userRoutes);
 server.register(boardRoute);
