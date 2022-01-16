a9d014117efad253860329cedaed9c936856a427
ratomsky <max.work.dev@gmail.com>
2022-01-16 17:29:41 +0300

    init: install typeorm

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,17 @@
+import 'reflect-metadata';
+import { createConnection } from 'typeorm';
+
 import server from './app';
 import config from './common/config';
+
+createConnection({
+  type: "postgres",
+  host: "localhost",
+  port: 5432,
+  username: "postgres",
+  password: "postgres",
+  database: 'postgres'
+})
 
 const start = async () => {
   try {
